a8d3e2ba8184bad534b04bf894afae1dee554145
jeep70 <jeep70cp@gmail.com>
2016-01-26 14:52:13 -0500

    be simple you fool

--- mc18/serverinfo/index.js
+++
@@ -1,8 +0,0 @@
-MinecraftAPI.getServerStatus('s7.hosthorde.com', {
-  port: 26297
-}, function(err, status) {
-  if (err) {
-    return document.querySelector('#motd').innerHTML = 'Error loading status';
-  }
-  console.log(motd);
-});
