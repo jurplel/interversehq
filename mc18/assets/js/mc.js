70ad1a31d1c1f1a0a4e3b27603b5bb05727e5514
jeep70 <jeep70cp@gmail.com>
2016-05-30 14:36:19 -0400

    boot

---
+++ mc18/assets/js/mc.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#modlist').DataTable( {
+		"scrollY": "65vh",
+		"scrollCollapse": true,
+		"paging": false,
+		"info": false,
+	} );
+} );
+
+$('.carousel').carousel({
+	interval: 5000,
+	pause: "false"
+})
