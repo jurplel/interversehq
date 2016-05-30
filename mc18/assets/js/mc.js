231574b026260f366ef19d8046a0573302ff99da
jeep70 <jeep70cp@gmail.com>
2016-05-29 20:44:22 -0400

    lets do it

--- mc18/assets/js/mc.js
+++ mc18/assets/js/mc.js
@@ -1,8 +1,15 @@
 $(document).foundation();
 
 var orbit = new Foundation.Orbit($('#example'), {animation: 'fade',
-	timerDelay: 8000,
-	pauseOnHover: true,
-	navButtons: false,
-	bullets: false
+    animation: 'fade',
+    timer_speed: 3000,
+    animation_speed: 400,
+    stack_on_small: false,
+    navigation_arrows: false,
+    slide_number: false,
+    pause_on_hover: false,
+    resume_on_mouseout: false,
+    bullets: false,
+    timer: true,
+    variable_height: false,
 });
