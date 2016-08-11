68125428fba57dd3348f5a22af4835d8768caf2d
jeep70 <jeep70cp@gmail.com>
2016-08-11 19:19:35 -0400

    Good stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
-var i = {
+var options = {
     file: '/assets/img/intv_logo_outline.svg',
     type: "async",
     duration: 150,
     pathTimingFunction: Vivus.EASE_OUT,
     animTimingFunction: Vivus.EASE
+    onReady function (logofadein) {
+    document.getElementById("filledlogo").style.opacity = "1";
+    }
 };
