7b17d784746def10df81a7bf557c6f7ae65be9ff
jeep70 <jeep70cp@gmail.com>
2016-08-11 19:24:36 -0400

    COMMAAAAAAA

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ var i = {
     type: "async",
     duration: 150,
     pathTimingFunction: Vivus.EASE_OUT,
-    animTimingFunction: Vivus.EASE
-	onReady: function (myVivus) {
-	}
+    animTimingFunction: Vivus.EASE,
+    onReady: function (logofadein) {
+    document.getElementById("filledlogo").style.opacity = "1";
+    }
 };
