4b19ea80a1e62210fd851b8a0dcbe57b88f87e16
jurplel <jeep70cp@gmail.com>
2017-11-30 10:54:32 -0500

    he's done it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,19 @@
-const mq = window.matchMedia( "(orientation: landscape)" );
-$(document).ready(function(){
-    $(window).resize(function(){
-        if (mq.matches) {
-            var f = '/assets/img/intv_logo_outline.svg';
-        } else {
-            var f = '/assets/img/intv_compact_outline.svg'
-        }
-        var i = {
-            file: f,
-            type: "async",
-            duration: 150,
-            pathTimingFunction: Vivus.EASE_OUT,
-            animTimingFunction: Vivus.EASE,
-        };
-    });
-});
+if (matchMedia) {
+    const mq = window.matchMedia("(orientation: landscape)");
+    mq.addListener(Define_i);
+    Define_i(mq)
+}
+function Define_i(mq) {
+    if (mq.matches) {
+        var f = '/assets/img/intv_logo_outline.svg';
+    } else {
+        var f = '/assets/img/intv_compact_outline.svg'
+    }
+    var i = {
+        file: f,
+        type: "async",
+        duration: 150,
+        pathTimingFunction: Vivus.EASE_OUT,
+        animTimingFunction: Vivus.EASE,
+    };
+}
