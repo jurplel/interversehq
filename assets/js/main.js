21a00e119d4850ec907a40987ed9493220c778a0
jurplel <jeep70cp@gmail.com>
2017-11-30 10:58:17 -0500

    trying something else

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+$('#filledlogo, #button1, #button2, #button3').hide()
+var i = {
+    file: '/assets/img/intv_compact_outline.svg',
+    type: "async",
+    duration: 150,
+    pathTimingFunction: Vivus.EASE_OUT,
+    animTimingFunction: Vivus.EASE,
+};
+
+var myVivus = new Vivus("logo",i,function () {
+    $('#filledlogo').fadeIn( 750 );
+    $('#button1, #button2, #button3').show(0).addClass('animated fadeInUp');
+});
+
 if (matchMedia) {
     const mq = window.matchMedia("(orientation: landscape)");
     mq.addListener(Define_i);
@@ -5,7 +19,7 @@ if (matchMedia) {
 }
 function Define_i(mq) {
     if (mq.matches) {
-        var f = '/assets/img/intv_logo_outline.svg';
+        var f = '/assets/img/intv_logo_outline.svg'
     } else {
         var f = '/assets/img/intv_compact_outline.svg'
     }
