a732edfc2101d76a6129ed56ca174779d48974e9
jurplel <jeep70cp@gmail.com>
2017-11-30 10:33:10 -0500

    did a js check and css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
-var i = {
-    file: '/assets/img/intv_compact_outline.svg',
-    type: "async",
-    duration: 150,
-    pathTimingFunction: Vivus.EASE_OUT,
-    animTimingFunction: Vivus.EASE,
-};
+const mq = window.matchMedia( "(orientation: landscape)" );
+$(document).ready(function(){
+    $(window).resize(function(){
+        if (mq.matches) {
+            var f = '/assets/img/intv_logo_outline.svg';
+        } else {
+            var f = '/assets/img/intv_compact_outline.svg'
+        }
+        var i = {
+            file: f,
+            type: "async",
+            duration: 150,
+            pathTimingFunction: Vivus.EASE_OUT,
+            animTimingFunction: Vivus.EASE,
+        };
+    });
+});
