7729ba11cf669f7e6d3726d2a947657a10f574d4
jurplel <jeep70cp@gmail.com>
2017-11-30 11:09:14 -0500

    let go ? im just trying

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,21 @@
-$('#filledlogo, #button1, #button2, #button3').hide()
+const mq = window.matchMedia("(orientation: landscape)");
+if (mq.matches) {
+    var f = '/assets/img/intv_logo_outline.svg'
+} else {
+    var f = '/assets/img/intv_compact_outline.svg'
+}
 var i = {
-    file: '/assets/img/intv_compact_outline.svg',
+    file: f,
     type: "async",
     duration: 150,
     pathTimingFunction: Vivus.EASE_OUT,
     animTimingFunction: Vivus.EASE,
 };
 
+$('#filledlogo, #button1, #button2, #button3').hide()
+
 var myVivus = new Vivus("logo",i,function () {
     $('#filledlogo').fadeIn( 750 );
     $('#button1, #button2, #button3').show(0).addClass('animated fadeInUp');
+    myVivus.delay(800).stop().reset()
 });
-
-if (matchMedia) {
-    const mq = window.matchMedia("(orientation: landscape)");
-    mq.addListener(Define_i);
-    Define_i(mq)
-}
-function Define_i(mq) {
-    if (mq.matches) {
-        var f = '/assets/img/intv_logo_outline.svg'
-    } else {
-        var f = '/assets/img/intv_compact_outline.svg'
-    }
-    var i = {
-        file: f,
-        type: "async",
-        duration: 150,
-        pathTimingFunction: Vivus.EASE_OUT,
-        animTimingFunction: Vivus.EASE,
-    };
-}
