376f5765c9ea9930f34f336a1c86e828e8ce9bea
Benjamin O <jeep70cp@gmail.com>
2017-12-03 23:30:44 -0500

    maintainance changes
    Former-commit-id: c2e51b34cfe6f5c771eef7917fa41f03150b4e63 [formerly e0ba89af41133c3082cc530edd54a52e66f906f1]
    Former-commit-id: 3b8fb672502935083a2922bfa8353e1de1a1c65e

---
+++ mc9/assets/js/mc.js
@@ -0,0 +1,35 @@
+$("#table").load("table.html", function() {
+	$('#modlist').DataTable( {
+		"scrollY": "65vh",
+		"scrollCollapse": true,
+		"paging": false,
+		"info": false,
+	} );
+} );
+
+$('.carousel').carousel({
+	interval: 5000,
+	pause: "false"
+});
+
+$(document).ready(function() {
+	$('#modlist').DataTable( {
+		"scrollY": "65vh",
+		"scrollCollapse": true,
+		"paging": false,
+		"info": false,
+	} );
+} );
+
+var toggle = false;
+$('#hiddenText').hide();
+$('#doSmth').click(function() {
+    if(toggle===false) {
+        $('#hiddenText').show();
+        toggle = true;
+    }
+    else {
+        $('#hiddenText').hide();
+        toggle = false;
+    }
+});
