ff002022a2a5a53400e4a8d3dd2883440b7eb9d8
jurplel <jeep70cp@gmail.com>
2017-12-10 21:28:17 -0500

    navbar outsourced
    Former-commit-id: 32172e8239fa5d8ca840338b5684586af87590d1 [formerly 3daa590446e4037dee1b40dacdb27604993228cc]
    Former-commit-id: f6afe1d0e1a597ad78a035c7fe7eb2aa35c0ef7a

---
+++ assets/js/nav.js
@@ -0,0 +1,33 @@
+var navbarHTML = `
+<nav class="navbar navbar-expand-sm navbar-fixed-top navbar-dark">
+<a href="#" class="navbar-brand"><img style="height: 30px;" src="assets/img/intv_compact.svg" class="d-inline-block align-top"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span></button>
+<div class="container-fluid">
+    <div class="navbar-collapse collapse">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="don't_tell_anybody_this_doesn't_go_anywhere">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="mc">MINECRAFT</a>
+            </li>
+        </ul>
+    </div>
+    <div class="navbar-collapse collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="http://steamcommunity.com/groups/interverse">STEAM</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://discord.gg/0lXeGE6soHgmQPT5">DISCORD</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>`
+
+var navbarCSS = `<link rel="stylesheet" href="assets/css/main.css">`
+
+$('body').append(navbarHTML)
+$('head').append(navbarCSS)
