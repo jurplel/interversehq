30ef300c7b269128af576aabff0de7a9f498a6d4
Benjamin O <jeep70cp@gmail.com>
2018-04-23 23:45:58 -0400

    try again
    Former-commit-id: 9d6e366aa474b477472e0a52da1bb2fb4b1e9557 [formerly 7d228a90700916aa79a76f6eeaa1a13e6b297bf3]
    Former-commit-id: f7936b93f6603f7a6bcbba9ec39f86f1d0a4672a

---
+++ qview/assets/js/qview.js
@@ -0,0 +1,84 @@
+$('#filledlogo').fadeIn( 750 );
+$('.navbar').addClass('animated fadeIn');
+$('header').addClass('animated fadeIn');
+
+$(".linuxlabel").hide();
+$(".dlgrp").hide();
+$("#ty").hide();
+
+var os = 3;
+if (navigator.platform.indexOf("Win") != -1)
+{
+    if (navigator.userAgent.indexOf("WOW64") != -1 || 
+        navigator.userAgent.indexOf("Win64") != -1 ){
+        $(".dlbtn").html("Download for Windows");
+        os=0;
+    } 
+    else {
+        $(".dlbtn").html("Download for Windows");
+        os=1;
+    }
+}
+else if (navigator.platform.indexOf("MacIntel") != -1)
+{
+    $(".dlbtn").html("Download for macOS");
+    os=2;
+}
+else if (navigator.platform.indexOf("Linux") != -1)
+{
+    $(".dlbtn").html("Download for Linux");
+    $(".linuxlabel").show();
+    os=3;
+}
+
+$(".dlbtn").click(function() {
+    $.getJSON("https://api.github.com/repos/jeep70/qView/releases", function(data) {
+        if (os == 0)
+        {
+            location.href=data[0].assets[0].browser_download_url;
+        }
+        else if (os == 1)
+        {
+            location.href=data[0].assets[1].browser_download_url;
+        }
+        else if (os == 2)
+        {
+            location.href=data[0].assets[2].browser_download_url;
+        }
+        else
+        {
+            location.href=data[0].tarball_url;
+        }
+        $("#ty").show().addClass('animated zoomIn');
+    });
+});
+
+$(".dlgrp1").click(function() {
+    $.getJSON("https://api.github.com/repos/jeep70/qView/releases", function(data) {
+        location.href=data[0].assets[0].browser_download_url;
+    });
+});
+
+$(".dlgrp2").click(function() {
+    $.getJSON("https://api.github.com/repos/jeep70/qView/releases", function(data) {
+        location.href=data[0].assets[1].browser_download_url;
+    });
+});
+
+$(".dlgrp3").click(function() {
+    $.getJSON("https://api.github.com/repos/jeep70/qView/releases", function(data) {
+        location.href=data[0].assets[2].browser_download_url;
+    });
+});
+
+$(".dlgrp4").click(function() {
+    $(".linuxlabel").show();
+    $.getJSON("https://api.github.com/repos/jeep70/qView/releases", function(data) {
+        location.href=data[0].tarball_url;
+    });
+});
+
+$("#dllabel").click(function() {
+    $(".dlbtn").toggle();
+    $(".dlgrp").toggle();
+});
