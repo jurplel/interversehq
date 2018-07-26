4af7b8b7407277e952f469658648089f0223c50c
Benjamin O <jeep70cp@gmail.com>
2018-07-26 17:22:34 -0400

    removed compact V on main page and drastically simplified the code because of that
    Former-commit-id: 0c0fd9aceefdb5debfbeb856e0d455a44792e7c5 [formerly 6faa3a30d08b88909e3abf710e41a9f262759168]
    Former-commit-id: ff9130487f0f3c10d51897b0640b17dc1614ad6e

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,62 +10,25 @@ function Entrance() {
     $('.navbar').css("visibility", "visible").addClass('animated fadeInDown');
 }
 
-if (Cookies.get('visited')) 
-{
-    Entrance();
-}
-else 
-{
-    Cookies.set('visited', 'yes', { path: '/' });
-    
-    var regularLogo = {
-        file: 'assets/img/intv_logo_outline.svg',
-        onReady: function (myVivus) {
-            const mq = window.matchMedia("(min-width: 576px)");
-            if (!mq.matches) {
-                $('#I_concept').hide();
-            }
-        },
-        type: "async",
-        duration: 150,
-        pathTimingFunction: Vivus.EASE_OUT,
-        animTimingFunction: Vivus.EASE,
-    };
-
-    var compactLogo = {
-        file: 'assets/img/intv_compact_outline.svg',
-        onReady: function (myVivus) {
-            const mq = window.matchMedia("(min-width: 576px)");
-            if (mq.matches) {
-                $('#Compact_Logo').hide();
-            }
-        },
-        type: "async",
-        duration: 150,
-        pathTimingFunction: Vivus.EASE_OUT,
-        animTimingFunction: Vivus.EASE,
-    };
-
-    var compactVivus = new Vivus("logo",compactLogo,function () {
+$( document ).ready(function() {
+    if (Cookies.get('visited')) 
+    {
         Entrance();
-    });
-
-    var regularVivus = new Vivus("logo",regularLogo,function () {
-        Entrance();
-    });
-
-    if (matchMedia) {
-        const mq = window.matchMedia("(min-width: 576px)");
-        mq.addListener(Define_i);
     }
+    else 
+    {
+        Cookies.set('visited', 'yes', { path: '/' });
+        
+        var regularLogo = {
+            file: 'assets/img/intv_logo_outline.svg',
+            type: "async",
+            duration: 150,
+            pathTimingFunction: Vivus.EASE_OUT,
+            animTimingFunction: Vivus.EASE,
+        };
 
-    function Define_i(mq) {
-        if (mq.matches) {
-            $('#I_concept').show();
-            $('#Compact_Logo').hide();
-        } else {
-            $('#I_concept').hide();
-            $('#Compact_Logo').show();
-        }
+        var regularVivus = new Vivus("logo",regularLogo,function () {
+            Entrance();
+        });
     }
-}
+})
