870bb18a497cf3b3c0f482d75af93f67a8da5792
Benjamin O <jeep70cp@gmail.com>
2018-11-24 02:32:23 -0500

    logo animation stuff
    Former-commit-id: eaef613955d348a320e22b56852aa04410fa356a [formerly 389daf5230ac9beac49a73d088a5c54cc4b9731b]
    Former-commit-id: 5f7efcbbafe1ac3889c9f69d87b3a69486df091e

---
+++ 2019/assets/js/main.js
@@ -0,0 +1,47 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+//start
+var logo = document.getElementById('logo');
+var logoText = document.getElementById('logotext');
+
+TweenMax.set(logo, {xPercent:-50, yPercent:-50, left:"50%", top:"50%"}) 
+TweenMax.set(logoText, {xPercent:-90, yPercent:-100, left:"50%", top:"50%", autoAlpha:"1"}) 
+
+window.onload = function() {
+    presentLogo();
+}
+
+function presentLogo() {
+    var newX = getRandomArbitrary(logo.x/4, logo.x/8);
+    var newY = getRandomArbitrary(logo.y/4, logo.y/8);
+
+    if (Math.random() > 0.5)
+        newX = -newX;
+    if (Math.random() > 0.5)
+        newY = -newY;
+
+    TweenLite.from(logo, 1.5, {
+        ease: Expo,
+        x: newX,
+        y: newY,
+        rotationY: newX > 0 ? 155 : -155,
+        rotationZ: newY > 0 ? 15 : -15,
+        autoAlpha: 0,
+        onComplete: presentText
+    });
+}
+
+function presentText() {
+    TweenLite.to(logo, 0.5, {
+        ease: Power2.easeInOut,
+        xPercent: -295,
+    });
+    TweenLite.to(logoText, 0.5, {
+        ease: Power2.easeInOut,
+        xPercent: -39,
+        clipPath: 'inset(0% 0% 0% 0%)',
+        webkitClipPath: 'inset(0% 0% 0% 0%)',
+    })
+}
