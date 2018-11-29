4a84e45992fa0aea8fc4875a78267ac8ad3fe42e
Benjamin O <jeep70cp@gmail.com>
2018-11-29 02:10:50 -0500

    sass, bulma, and bg stuff i guess
    Former-commit-id: 752cc68161d83c7397c895fbc033ca60f8a5c94f [formerly e556944f239125af213ea0875dcb555d2834190e]
    Former-commit-id: dc88b6ef31c6401060b6713706eb3744c758cf0b

--- 2019/assets/js/main.js
+++ 2019/assets/js/main.js
@@ -7,7 +7,7 @@ var logo = document.getElementById('logo');
 var logoText = document.getElementById('logotext');
 
 TweenMax.set(logo, {xPercent:-50, yPercent:-50, left:"50%", top:"50%"}) 
-TweenMax.set(logoText, {xPercent:-90, yPercent:-100, left:"50%", top:"50%", autoAlpha:"1"}) 
+TweenMax.set(logoText, {xPercent:-90, yPercent:-50, left:"50%", top:"50%", autoAlpha:"1"}) 
 
 window.onload = function() {
     presentLogo();
