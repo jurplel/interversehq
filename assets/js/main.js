621c1ab91aa3a5af55fc9f5556d46fbdde2e9d0e
Benjamin O <jeep70cp@gmail.com>
2019-01-31 23:38:25 -0500

    card reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,12 @@ function isVisible(e) {
 var logo = document.getElementById("logo");
 var logoText = document.getElementById("logotext");
 var backgroundBit = document.getElementById("backgroundbit");
+var qview = document.getElementById("qview");
 
 
 TweenLite.set(logo, {xPercent:-50, yPercent:-50, autoAlpha:"1"})
 TweenLite.set(logoText, {xPercent:-90, yPercent:-50, autoAlpha:"1"})
+TweenLite.set(qview, {rotationX:90})
 
 window.onload = function() {
     presentLogo();
@@ -52,3 +54,29 @@ function presentText() {
         autoAlpha: 1,
     });
 }
+
+const config = {
+    threshold: 1
+};
+  
+const tl = new TimelineLite()
+
+let observer = new IntersectionObserver(function(entries, self) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        let overlap = '-=0.3';
+        
+        if (!tl.isActive()) {
+            overlap = '+=0';
+        }
+        
+        tl.to(entry.target, 0.6, { 
+            ease: Back.easeOut,
+            rotationX:0
+         }, overlap);
+        self.unobserve(entry.target);
+        }
+    });
+}, config);
+
+observer.observe(qview);
