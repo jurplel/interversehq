6ececf335bce69d3dfe912222db989f36957c8fd
Benjamin O <jeep70cp@gmail.com>
2019-01-31 22:33:37 -0500

    new intro anim

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,22 @@ var logo = document.getElementById("logo");
 var logoText = document.getElementById("logotext");
 var backgroundBit = document.getElementById("backgroundbit");
 
-var isInfoShown = false;
-var currentInfoIndex = 0;
 
-TweenLite.set(logo, {xPercent:-50, yPercent:-50})
-TweenLite.set(logoText, {xPercent:-90, yPercent:-50, autoAlpha:"1"}) 
+TweenLite.set(logo, {xPercent:-50, yPercent:-50, autoAlpha:"1"})
+TweenLite.set(logoText, {xPercent:-90, yPercent:-50, autoAlpha:"1"})
 
 window.onload = function() {
     presentLogo();
 }
 
 function presentLogo() {
-    var newX = getRandomArbitrary(logo.x/4, logo.x/8);
-    var newY = getRandomArbitrary(logo.y/4, logo.y/8);
 
-    if (Math.random() > 0.5)
-        newX = -newX;
-    if (Math.random() > 0.5)
-        newY = -newY;
-
-    TweenLite.from(logo, 1.5, {
-        ease: Power1.easeOut,
-        x: newX,
-        y: newY,
-        rotationY: newX > 0 ? 155 : -155,
-        rotationZ: newY > 0 ? 15 : -15,
+    TweenLite.from(logo, 1, {
+        delay: 0.2,
+        ease: Power2.easeOut,
+        scale: 4,
+        rotationX: 90,
+        rotationZ: getRandomArbitrary(-120, 120),
         autoAlpha: 0,
         onComplete: presentText
     });
@@ -49,50 +40,15 @@ function presentText() {
     TweenLite.to(logoText, 0.5, {
         ease: Power2.easeInOut,
         xPercent: -36,
+    })
+    TweenLite.to(logoText, 0.48, {
+        ease: Power2.easeInOut,
         clipPath: "inset(0% 0% 0% 0%)",
         webkitClipPath: "inset(0% 0% 0% 0%)",
-        onComplete: presentAfter
     })
-}
 
-function presentAfter() {
-    TweenLite.to(backgroundBit, 1.0, {
-        ease: Power0.easeInOut,
+    TweenLite.to(backgroundBit, 1, {
+        ease: Power1.easeInOut,
         autoAlpha: 1,
     });
-}
-
-function setLogoPosition(setRaised) {
-    if (setRaised)
-    {
-        TweenLite.to([logo, logoText], 0.6, {
-            ease: Power2.easeInOut,
-            top: "20%"
-        })
-        TweenLite.to([projectsInfo, socialInfo], 0.6, {
-            ease: Power2.easeInOut,
-            top: "55%"
-        });
-        TweenLite.to(backgroundBit, 0.6, {
-            ease: Power2.easeInOut,
-            height: "45vh"
-        });
-        isInfoShown = true;
-    }
-    else
-    {
-        TweenLite.to([logo, logoText], 0.6, {
-            ease: Power2.easeInOut,
-            top: "45%"
-        })
-        TweenLite.to([projectsInfo, socialInfo], 0.6, {
-            ease: Power2.easeInOut,
-            top: "100%"
-        });
-        TweenLite.to(backgroundBit, 0.6, {
-            ease: Power2.easeInOut,
-            height: "100vh"
-        });
-        isInfoShown = false;
-    }
 }
