91672ecde99786d218161a5c04e0b519549d2400
Benjamin O <jeep70cp@gmail.com>
2020-07-07 15:44:10 -0400

    Fix up website more

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,41 +7,31 @@ function isVisible(e) {
 }
 
 //start
-// var logo = document.getElementById("logo");
 var logoText = document.getElementById("logotext");
-// var logoBg = document.getElementById("logobg");
 var backgroundBit = document.getElementById("backgroundbit");
-var canvas = document.getElementById("canvas");
 var navbar = document.getElementById("navbar");
 var qviewSectionLeft = document.getElementById("qviewsectionleft");
-var contact1 = document.getElementById("contact1");
-var contact2 = document.getElementById("contact2");
-var contact3 = document.getElementById("contact3");
-contact1.isContact = true;
-contact2.isContact = true;
-contact3.isContact = true;
 
 let logos = document.getElementsByClassName('logo');
-TweenLite.set(logos[0], {color: "rgba(255, 0, 0)"});
-TweenLite.set(logos[1], {color: "rgba(0, 255, 0)"});
-TweenLite.set(logos[2], {color: "rgba(0, 0, 255)"});
+TweenLite.set(logos[1], {color: "rgba(255, 0, 0)"});
+TweenLite.set(logos[2], {color: "rgba(0, 255, 0)"});
+TweenLite.set(logos[3], {color: "rgba(0, 0, 255)"});
 
-TweenLite.set(logos[0], {xPercent:-55, yPercent:-52, autoAlpha:"0"});
-TweenLite.set(logos[1], {xPercent:-50, yPercent:-50, autoAlpha:"0"});
-TweenLite.set(logos[2], {xPercent:-45, yPercent:-48, autoAlpha:"0"});
+TweenLite.set(logos[0], {xPercent:-50, yPercent:-50, autoAlpha:"0"});
+TweenLite.set(logos[1], {xPercent:-55, yPercent:-52, autoAlpha:"0"});
+TweenLite.set(logos[2], {xPercent:-50, yPercent:-50, autoAlpha:"0"});
+TweenLite.set(logos[3], {xPercent:-45, yPercent:-48, autoAlpha:"0"});
 
 TweenLite.set(logoText, {xPercent:-50, yPercent:-50, autoAlpha:"1"});
 
 TweenLite.set(navbar, {autoAlpha:"0"});
-
-// TweenLite.set(canvas, {autoAlpha:"0"});
 
 window.onload = function() {
     presentLogo();
 }
 
 function presentLogo() {
-    for (let i = 0; i < logos.length; i++) {
+    for (let i = 1; i < logos.length; i++) {
         TweenLite.to(logos[i], 1, {
             delay: 0.2,
             ease: Power2.easeOut,
@@ -50,7 +40,7 @@ function presentLogo() {
     }
     let offsetx = getRandomArbitrary(0, 50);
     let offsety = getRandomArbitrary(-50, 50);
-    TweenLite.from(logos[0], 1, {
+    TweenLite.from(logos[1], 1, {
         delay: 0.2,
         ease: Power2.easeOut,
         scale: 4,
@@ -58,69 +48,60 @@ function presentLogo() {
         xPercent: -offsetx-100,
         yPercent: -offsety-100,
     });
-    TweenLite.from(logos[1], 1, {
+    TweenLite.from(logos[2], 1, {
         delay: 0.2,
         ease: Power2.easeOut,
         scale: 4,
         color: "rgba(0, 255, 0)",
     });
-    TweenLite.from(logos[2], 1, {
+    TweenLite.from(logos[3], 1, {
         delay: 0.2,
         ease: Power2.easeOut,
         scale: 4,
         color: "rgba(0, 0, 255)",
         xPercent: offsetx,
         yPercent: offsety,
-        onComplete: presentText
+        onComplete: presentText0
     });
 }
 
-function presentText() {
+function presentText0() {
     TweenLite.to(logoText, 0.35, {
         delay: 0.2,
         ease: Power2.easeOut,
         backgroundPosition: "0%, 0%",
         onComplete: presentText1
     })
+    // presentText1();
 }
 
 function presentText1() {
-    // for (let i = 0; i < logos.length; i++) {
-    //     TweenLite.set(logos[i], {autoAlpha: "0"})
-    // }
-
     TweenLite.set(logoText, {color: "white"})
     TweenLite.to(logoText, 0.35, {
         ease: Power2.easeOut,
         backgroundPosition: "-98%, 0%",
     })
 
-    var roughEase = RoughEase.ease.config({ template:
-        Power1. easeIn,
-        strength:
-        1,
-        points:
-        20,
-        taper:
-        "none",
-        randomize: true,
-        clamp: true
-        })
-
     TweenLite.to(backgroundBit, 1.7, {
         ease: Power2.easeOut,
         autoAlpha: 1,
     });
-    TweenLite.from(backgroundBit, 1.7, {
+    TweenLite.to(logos[0], 2, {
         ease: Power2.easeOut,
-        scale: 0.01,
+        autoAlpha: 1,
+    });
+    TweenLite.to(backgroundBit, 2, {
+        ease: Power2.easeOut,
+        autoAlpha: 1,
     });
 
     TweenLite.to(navbar, 1, {
-        ease: roughEase,
+        ease: Power1.easeOut,
         autoAlpha: 1
     });
 }
+
+// Timeline and observer bits just for the qview section currently
 
 const config = {
     threshold: 0.3
@@ -136,31 +117,16 @@ let observer = new IntersectionObserver(function(entries, self) {
             if (!tl.isActive()) {
                 overlap = '+=0';
             }
-            if (entry.target.isContact) {
-                tl.to(entry.target, 1, { 
-                    ease: Back.easeOut,
-                    scale:1,
-                    rotationX:0
-                }, overlap);
-            }
-            else {
-                TweenLite.to(entry.target, 2, { 
-                    ease: Power4.easeOut,
-                    yPercent:0,
-                    xPercent:0,
-                    autoAlpha:1
-                }, overlap);
-            }
+            TweenLite.to(entry.target, 2, { 
+                ease: Power4.easeOut,
+                yPercent:0,
+                xPercent:0,
+                autoAlpha:1
+            }, overlap);
             self.unobserve(entry.target);
         }
     })}, config);
 
 TweenLite.set(qviewSectionLeft, {yPercent:30, autoAlpha:0})
-TweenLite.set(contact1, {rotationX:-90, scale:0})
-TweenLite.set(contact2, {rotationX:-90, scale:0})
-TweenLite.set(contact3, {rotationX:-90, scale:0})
 
 observer.observe(qviewSectionLeft);
-observer.observe(contact1);
-observer.observe(contact2);
-observer.observe(contact3);
